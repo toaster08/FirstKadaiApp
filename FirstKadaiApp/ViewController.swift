1c327a530a5c4a388c8d68c02347544938686491
toaster08 <y10star.hon@gmail.com>
2020-12-26 01:01:47 +0900

    課題提出用

---
+++ FirstKadaiApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp
+//
+//  Created by 山田　天星 on 2020/12/13.
+//  Copyright © 2020 山田　天星. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBOutlet weak var lblTitle: UILabel!
+    
+}
+
